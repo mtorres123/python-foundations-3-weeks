1a2f94ba81730175c31c9ee1a4b7e4c64444c805
Marissa J. Torres <mjtorres.usace@gmail.com>
2022-08-05 19:03:19 -0400

    started hw1

--- homework/homework_1.py
+++ homework/homework_1.py
@@ -1,17 +1,31 @@
 # Todo: Create a c_to_f(temp) function
-
+def c_to_f_temp(temp_c):
+    temp_f = (temp_c * 9/5) + 32
+    return(temp_f)
 
 # Todo: Create a list of reminders
-
+reminders = ['Dr. appt @ 3pm','Softball @ 6pm','Grocery shopping']
 
 # Todo: Get some input from the user
-
+gratuity = input("What are you most grateful for today? --> ")
 
 # Todo: Create a string with email content that includes:
 #       - A greeting
-#       - The day's weather + temp
 #       - Some reminders
 #       - 2-3 more items based on APIs that you've found
+content = """
+----------------------------------
+Good morning, Mar. Hoy va a ser un buen dia!
 
+Today you're grateful for {gratuity}. 
+
+~~ Weather ~~
+
+~~ Reminders ~~
+
+~~ API STUFF ~~
+
+"""
 
 # Todo: Print the content to the console
+print(content)
