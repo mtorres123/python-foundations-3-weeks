fef5bbaba35a4386bf3e1970bd25f82454ff7a54
mtorres123 <31076458+mtorres123@users.noreply.github.com>
2022-08-11 15:54:57 -0400

    final interaction for week 2

--- main.py
+++ main.py
@@ -3,3 +3,18 @@ from art import tprint
 text = input("Input some text: ")
 
 tprint(text)
+
+
+# --- 
+# this example exists in examples/week_2/23
+# ---
+
+# import sys
+# args = sys.argv
+# if len(args) > 1:
+#     name = args[1]
+    
+# else:
+#     name = input("Name: ")
+
+# print(f'hello {name}')
